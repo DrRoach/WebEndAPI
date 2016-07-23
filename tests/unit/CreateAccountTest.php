95e25fb1de39c26e8bf988163fee429ab41989fe
Ryan Deas <ryandeas1@gmail.com>
2016-07-23 02:15:40 +0100

    Updated tests

--- tests/unit/CreateAccountTest.php
+++ tests/unit/CreateAccountTest.php
@@ -32,11 +32,14 @@ class CreateAccountTest extends \Codeception\Test\Unit
         $user = [
             'username' => 'TestAccount',
             'password' => 'p4ssw0rd!',
+            'passwordConfirm' => 'p4ssw0rd!',
             'email' => 'testemail@gmail.com',
             'tel_number' => '0123456789'
         ];
 
-        Account::create($user);
+        $create = Account::create($user);
+
+        $this->assertTrue($create['success']);
 
         $data = \WebEndAPI\Database\Database::get([
             'table' => 'users',
@@ -49,16 +52,20 @@ class CreateAccountTest extends \Codeception\Test\Unit
         $this->assertEquals('testemail@gmail.com', $data['email']);
         $this->assertEquals('0123456789', $data['tel_number']);
 
-        $user['username'] = 'TestAccount\'';
-
+        /**
+         * Test that usernames with special characters in aren't saved
+         */
+        $user['username'] = 'TestAccount\' OR 1=\'1';
         $falseResult = Account::create($user);
-
         $this->assertFalse($falseResult['success']);
 
+        /**
+         * Test that duplicate account names aren't saved
+         */
         $user['username'] = 'TestAccount';
-
         $usernameInUse = Account::create($user);
-
         $this->assertEquals('Sorry that username has already been taken.', $usernameInUse['message']);
+
+
     }
 }
